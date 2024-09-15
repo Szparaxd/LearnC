5d128d367809c481d1fa681c8ef3616e2df1a80a
Lorek Adam <szparax@gmail.com>
2024-09-15 14:06:40 +0200

    save

--- calculator/main.c
+++ calculator/main.c
@@ -1,94 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
 // zmienna  - zmienna
 // &zmienna - adress do zmiennej
 
 // '\0'     - znak końca stringa
 
-int sum(int a, int b);
-int sub(int a, int b);
-
-void clearArray(char array[]);
-int action(int a, int b, char action);
+float action(float a, float b, char action);
+int arrayContain(char array[], char value);
+char *replace_substring(char str[], char oldStr[], char newStr[]);
+int findEndIndex(char str[]);
 
 int main()
 {
     printf("#################################### \n");
 
-    char actions[] = {'+', '-'};
-    char lastAction = '+';
-    // char operation[100];
-    // printf("Dzialanie: ");
-    // scanf("%s", operation); // Spacja przed %c pomija białe znaki
+    char str[] = "aoaoaoaoaoa";
+    char prev[] = "o";
+    char new[] = "0";
+    char *repStr = replace_substring(str, prev, new);
 
-    // test
-    char operation[] = "11+22+33-66";
-    printf("Dzialanie: %s\n", operation);
+    printf("%s \n", repStr);
 
-    int i = 0, cellIndex = 0, a = 0, b = 0;
-    char cell[] = {'\0', '\0', '\0', '\0'};
-
-    int length = sizeof(operation) / sizeof(operation[0]);
-    for (i = 0; i < length; i++)
-    {
-        if ((operation[i] != '+' || operation[i] != '-') &&  operation[i] != '\0')
-        {
-            cell[cellIndex] = operation[i];
-            cellIndex++;
-            continue;
-        }
-
-        if(operation[i] != '\0')
-            lastAction = operation[i];
-
-        cellIndex = 0;
-
-        if (a != 0)
-            b = atoi(cell);
-        else
-            a = atoi(cell);
-
-        if (a != 0 && b != 0)
-        {
-            a = action(a, b, lastAction);
-            b = 0;
-        }
-
-        clearArray(cell);
-    }
-
-    printf("Dzialanie: %s wynik= %d", operation, a);
     return 0;
 }
 
-int action(int a, int b, char action)
+char *replace_substring(char str[], char oldStr[], char newStr[])
+{
+    char result[900] = "";
+
+    int strLength = sizeof(str) / sizeof(str[0]);
+    printf("%d \n", strLength);
+
+    int rStrLength = sizeof(result) / sizeof(result[0]);
+    printf("%d \n", rStrLength);
+
+    for (int i = 0; i < strLength; i++)
+    {
+
+    }
+
+    return "abc";
+}
+
+int findEndIndex(char str[])
+{   
+    int i =0;
+    for (i = 0; str[i] != '\0'; i++)
+    { }
+    return i;
+}
+
+char addString(char str[], char strToAdd[])
+
+
+float action(float a, float b, char action)
 {
     switch (action)
     {
     case '+':
-        return sum(a, b);
+        return a + b;
     case '-':
-        return sub(a,b);
+        return a - b;
+    case '*':
+        return a * b;
+    case '/':
+        return a / b;
     default:
-        return -99;
+        printf("action: %c \n", action);
+        return -99.0;
     }
 }
 
-int sum(int a, int b)
-{
-    return a + b;
-}
-
-int sub(int a, int b)
-{
-    return a - b;
-}
-
-void clearArray(char array[])
+int arrayContain(char array[], char value)
 {
     for (int i = 0; array[i] != '\0'; i++)
     {
-        array[i] = '\0';
+        if (array[i] == value)
+            return true;
     }
+
+    return false;
 }
